af0a5473b33eca9427bf23bee4ce51812857894c
Hithesh G R <hithu.gowda2@gmail.com>
2020-08-09 19:43:14 +0530

    Updates

---
+++ precache-manifest.60873b4bed280ab9b22ec3dff32ab3eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291e5dd1417e63bf86a5c86e51052648",
+    "url": "/react-deploy-mvApp/index.html"
+  },
+  {
+    "revision": "6d6bc89ac76463010620",
+    "url": "/react-deploy-mvApp/static/css/main.1fb3244b.chunk.css"
+  },
+  {
+    "revision": "881967c95dd658d1864b",
+    "url": "/react-deploy-mvApp/static/js/2.03713fab.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/react-deploy-mvApp/static/js/2.03713fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6bc89ac76463010620",
+    "url": "/react-deploy-mvApp/static/js/main.ebad25fe.chunk.js"
+  },
+  {
+    "revision": "4f296278ded32cdb2995",
+    "url": "/react-deploy-mvApp/static/js/runtime-main.4b90b9bd.js"
+  },
+  {
+    "revision": "797faff767eec9ae3bc22bac3ac5d6c9",
+    "url": "/react-deploy-mvApp/static/media/movieclip.797faff7.jpg"
+  }
+]);
